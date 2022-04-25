1d4450ce51fe76e82bdae984fee145d1a388d721
Miqueias Soares <mqseraos@gmail.com>
2022-04-24 21:28:30 -0300

    create finish responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,14 @@
-const showMenu = document.querySelectorAll('.toggle');
+const showMenu = document.querySelectorAll('.open');
+const closeMenu = document.querySelectorAll('.close');
 const nav = document.querySelector('nav.menu');
 
 for (const element of showMenu) {
-    element.addEventListener('click', function(){
-        nav.classList.toggle('active');
+    element.addEventListener('click', function() {
+        nav.classList.add('active');
     })
-}
+};
+for (const element of closeMenu) {
+    element.addEventListener('click', function() {
+        nav.classList.remove('active');
+    })
+};
