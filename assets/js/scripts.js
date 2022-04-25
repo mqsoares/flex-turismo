8ec56ff2ea419d36d5b287f1c456c61fe6cb1772
Miqueias Soares <mqseraos@gmail.com>
2022-04-24 21:47:29 -0300

    create finish btn mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,14 @@
 const showMenu = document.querySelectorAll('.open');
-const closeMenu = document.querySelectorAll('.close');
+const closeMenu = document.querySelectorAll('.close, nav.menu ul li a, header a');
 const nav = document.querySelector('nav.menu');
+
 
 for (const element of showMenu) {
     element.addEventListener('click', function() {
         nav.classList.add('active');
     })
 };
+
 for (const element of closeMenu) {
     element.addEventListener('click', function() {
         nav.classList.remove('active');
